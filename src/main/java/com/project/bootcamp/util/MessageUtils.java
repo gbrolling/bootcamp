060d936636dbf9f961a7b94031358005b5c885ae
gbrolling <giovanni_1733@hotmail.com>
2021-05-28 22:35:43 -0300

    criação de repośitório com o código do bootcamp

---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,9 @@
+package com.project.bootcamp.util;
+
+
+public abstract class MessageUtils {
+
+    public static final String STOCK_ALREADY_EXIST = "Stock already exist in the database";
+    public static final String NO_RECORDS_FOUND = "NO RECORDS FOUND";
+
+}
